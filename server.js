4a6f8a2af1fb756a59268a9849cc086cb2857f63
JesusMtnez <jmartinez@conwet.com>
2015-02-05 18:03:04 +0100

    Add first method to collect data from recieved request.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- server.js
+++ server.js
@@ -3,7 +3,23 @@
  * Last edit: 05 February 2015
  */
 
+/* Requires */
 var xmlhttprequest = require('./lib/xmlhttprequest').XMLHttpRequest;
 var express = require('express');
 
+/* Init app with express framework */
 var app = express();
+
+app.use(function(request, respond, next) {
+	var data='';
+	request.setEncoding('utf8');
+	// Save data
+	request.on('data', function(chunk) {
+		data += chunk;
+	});
+	// Finish recieving data
+	resquest.on('end', function() {
+		request.body = data;
+		next();
+	});
+});

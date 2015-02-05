12561776e9553e4ae0c1e7bfcfcf87bdeb9b215a
JesusMtnez <jmartinez@conwet.com>
2015-02-05 17:58:25 +0100

    Init app with Express framework.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- server.js
+++ server.js
@@ -2,3 +2,8 @@
  * Author: Jesús Martínez-Barquero Herrada
  * Last edit: 05 February 2015
  */
+
+var xmlhttprequest = require('./lib/xmlhttprequest').XMLHttpRequest;
+var express = require('express');
+
+var app = express();

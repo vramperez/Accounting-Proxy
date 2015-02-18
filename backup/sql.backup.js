d2e98c8ed6411e766b5fc7af1c9647538ae91b2a
JesusMtnez <jmartinez@conwet.com>
2015-02-18 16:41:41 +0100

    Implement 'loadFromDB' to load information from DB.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- backup/sql.backup.js
+++ backup/sql.backup.js
@@ -12,15 +12,32 @@ var connection = mysql.createConnection({
     password: MYSQL_PASS,
 });
 
-exports.init = function() {
-    connection.connect();
+/**
+ * Load accounting info from DB
+ */
+exports.loadFromDB = function(setMap) {
+    var toReturn = {};
+    connection.query('SELECT * FROM counts', function(err, results) {
+        if (results.length !== 0)
+            for (i in results)
+                toReturn[results[i].nickname] = results[i].requests;
+        else
+            console.log('[LOG] No data avaliable.');
+        setMap(toReturn);
+        delete toReturn;
+   });
+}
 
-    /* Create Database */
+/**
+ * Connect to database server and create everything if it is necessary.
+ */
+exports.init = function(setMap) {
+    connection.connect();
+    // Create Database if not exists
     connection.query('CREATE SCHEMA IF NOT EXISTS AccountingDDBB CHARACTER SET utf8 COLLATE utf8_general_ci');
-    /* Stablish the database to be used */
-    connection.query('USE AccountingDDBB')
-    connection.query('DROP TABLE IF EXISTS counts');
-    /* Add a new table */
+    // Stablish the database to be used
+    connection.query('USE AccountingDDBB');
+    // Add a new table
     connection.query('CREATE TABLE IF NOT EXISTS counts ( \
                      nickname    VARCHAR(20), \
                      requests    INTEGER, \
@@ -28,33 +45,26 @@ exports.init = function() {
                      ) ENGINE=InnoDB');
 }
 
-exports.saveUsers = function(counts) {
-    for(user in counts) {
-        if (counts[user] == 1) {
-            connection.query("INSERT INTO counts VALUE (?,?)", [user,counts[user]]);
-        }
-        else {
-            connection.query("UPDATE counts SET requests=? WHERE nickname=?",[counts[user], user]);
-        }
+/**
+ * Add/Update number of request of a user.
+ * @param  {INTEGER}    numReq [number of requests]
+ * @param  {STRING}     user   [user name]
+ */
+exports.save = function(numReq, user) {
+    if (numReq == 1) {
+        connection.query("INSERT INTO counts VALUE (?,?)", [user,numReq]);
+    }
+    else {
+        connection.query("UPDATE counts SET requests=? WHERE nickname=?",[numReq, user]);
     }
 }
 
-exports.close = function() {connection.end();}
-/* Add info 
-/*connection.query("INSERT INTO Sucursal_G1 \
-                 VALUE ('S1','Paseo de la Castellana','916650765')"
-                 );
-connection.query("INSERT INTO Sucursal_G1 \
-                 VALUE ('S2','Avenida de Boadilla','916650765')"
-                 );
-connection.query("INSERT INTO Sucursal_G1 \
-                 VALUE ('S3','Paseo del Prado','916650765')"
-                 );
-connection.query("INSERT INTO Sucursal_G1 \
-                 VALUE ('S4','Ronda de Valencia','916650765')"
-                 );
-connection.query("INSERT INTO Sucursal_G1 \
-                 VALUE ('S5','Recoletos','916650765')"
-                 );*/
+/**
+ * Disconnect from DB server.
+ */
+exports.close = function() {
+    connection.end();
+}
+
 // https://github.com/patriksimek/node-mssql
 // https://github.com/felixge/node-mysql

0dd5e20d4d149b5f590f86226652573883f3b2d8
JesusMtnez <jmartinez@conwet.com>
2015-03-03 18:07:25 +0100

    Fix only POST method in notification server and check request url.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- server2.js
+++ server2.js
@@ -21,30 +21,35 @@ exports.run = function(){
     app.listen(app.get('port'));
 }
 
-app.use(function(request, response, next) {
-    request.setEncoding('utf-8');
-    var body = '';
-    if (request.get('Content-Type') === 'application/json') {
-        request.on('data', function(data) {
-            body += data;
-        });
+app.post('/notifications/buyers',function(request, response, next) {
+    if (request.originalUrl !== '/notifications/buyers')
+        response.status(404).send('Cannot POST ' + request.originalUrl);
+    else {
+        console.log("NEW USER RECIEVED");
+        request.setEncoding('utf-8');
+        var body = '';
+        if (request.get('Content-Type') === 'application/json') {
+            request.on('data', function(data) {
+                body += data;
+            });
 
-        request.on('end', function() {
-            // TODO: JSON.parse err
-            body = JSON.parse(body);
-            var r = body.resources;
-            // Checks proxy's resource in the request resources list
-            for (i in r) {
-                if (r[i].name === resource.name
-                    && r[i].version === resource.version
-                    && r[i].url === resource.url) {
-                    console.log("[LOG] Resource OK!");
-                    mainSrv.newUser(body.customer, body.customer_name, body.reference);
-                    return;
+            request.on('end', function() {
+                // TODO: JSON.parse err
+                body = JSON.parse(body);
+                var r = body.resources;
+                // Checks proxy's resource in the request resources list
+                for (i in r) {
+                    if (r[i].name === resource.name
+                        && r[i].version === resource.version
+                        && r[i].url === resource.url) {
+                        console.log("[LOG] Resource OK!");
+                        mainSrv.newUser(body.customer, body.customer_name, body.reference);
+                        return;
+                    }
                 }
-            }
-            console.log("[LOG] Resource FAIL!");
-        });
+                console.log("[LOG] Resource FAIL!");
+            });
+        }
+        response.send('Server 2');
     }
-    response.send('Server 2');
 });

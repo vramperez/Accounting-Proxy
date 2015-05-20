653c05149b7f2211186c1b3b2223b5e8129146f2
JesusMtnez <jmartinez@conwet.com>
2015-05-20 17:19:13 +0200

    Add route in API_Server for new users.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- APIServer.js
+++ APIServer.js
@@ -1,32 +1,72 @@
-/**
- * Author: Jesús Martínez-Barquero Herrada
- * Last edit: 06 March 2015
- */
-
-/* Requires */
 var express = require('express');
 var mainSrv = require('./server');
 var resource = require('./config').resource;
+var db = require('./db.js');
 
-/* Create app with Express Framework */
-var app = express();
+var app = express(),
+    router = express.Router(),
+    resources = [];
 
 app.set('port', 9001);
 
-/**
- * Start server listening.
- */
 exports.run = function(){
-    // Listening at port 9001
-    app.listen(app.get('port'));
-}
+    db.loadResources(function(res) {
+        resources = res;
+        // console.log(JSON.stringify(resources, null, 2));
+        app.listen(app.get('port'));
+    });
+};
+
+router.post('/users', function(req, res) {
+    console.log("[LOG] WStore notification recieved.");
+    req.setEncoding('utf-8');
+    var body = '';
+    if (req.get('Content-Type') === 'application/json') {
+        req.on('data', function(data) {
+            body += data;
+        });
+        req.on('end', function() {
+            body = JSON.parse(body);
+            var offer = body.offering,
+                resrc = body.resources,
+                user  = body.customer,
+                ref   = body.reference;
+            db.getResources(offer.organization, offer.name, offer.version, function(data) {
+                if (data) {
+                    for (i in data) {
+                        for (j in resrc) {
+                            if (data[i].provider === resrc[j].provider &&
+                                data[i].name === resrc[j].name &&
+                                data[i].version === resrc[j].version) {
+                                console.log('FOUND!!');
+                                db.addUser(user, ref, resrc[j], offer);
+                            }
+                        }
+                    }
+                } else {
+                    console.log("New offer");
+                    for (i in resources) {
+                        for (j in resrc) {
+                            if (resources[i].provider === resrc[j].provider &&
+                                resources[i].name === resrc[j].name &&
+                                resources[i].version == resrc[j].version) {
+                                console.log('found!!');
+                                // TODO: Update DB
+                            }
+                        }
+                    }
+                }
+            });
+            // console.log(JSON.stringify(offer, null, 2));
+            res.send("API Server Woking!");
+        });
+    }
+});
 
 app.post('/notifications/buyers',function(request, response, next) {
     if (request.originalUrl !== '/notifications/buyers')
         response.status(404).send('Cannot POST ' + request.originalUrl);
     else {
-        console.log("[LOG] WStore notification recieved.");
-        request.setEncoding('utf-8');
         var body = '';
         if (request.get('Content-Type') === 'application/json') {
             request.on('data', function(data) {
@@ -53,3 +93,5 @@ app.post('/notifications/buyers',function(request, response, next) {
         response.send('Server 2');
     }
 });
+
+app.use('/api', router);

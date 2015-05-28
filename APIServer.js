d73c0ba45ae6f7e57d56a976c5814c641ede1a2c
JesusMtnez <jmartinez@conwet.com>
2015-05-28 16:26:27 +0200

    Update '.gitignore' and minor style fix in 'APIServer.js'.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- APIServer.js
+++ APIServer.js
@@ -18,21 +18,28 @@ exports.run = function(){
 };
 
 router.post('/users', function(req, res) {
+
     console.log("[LOG] WStore notification recieved.");
     req.setEncoding('utf-8');
+
     var body = '';
+
     if (req.get('Content-Type') === 'application/json') {
         req.on('data', function(data) {
             body += data;
         });
+
         req.on('end', function() {
+
             body = JSON.parse(body);
+
             var offer = body.offering,
                 resrc = body.resources,
                 user  = body.customer,
                 ref   = body.reference,
                 temRes = [],
                 apiKey;
+
             db.getResources(offer.organization, offer.name, offer.version, function(data) {
                 if (data) {
                     for (i in data) {
@@ -40,7 +47,6 @@ router.post('/users', function(req, res) {
                             if (data[i].provider === resrc[j].provider &&
                                 data[i].name === resrc[j].name &&
                                 data[i].version === resrc[j].version) {
-                                // console.log('FOUND!!');
                                 temRes.push(resrc[j]);
                             }
                         }
@@ -52,58 +58,26 @@ router.post('/users', function(req, res) {
                             if (resources[i].provider === resrc[j].provider &&
                                 resources[i].name === resrc[j].name &&
                                 resources[i].version == resrc[j].version) {
-                                // console.log('found!!');
                                 temRes.push(resrc[j]);
                             }
                         }
                     }
                 }
-                console.log(temRes);
+
                 db.getApiKey(user, offer, ref, function(API_KEY) {
                     apiKey = API_KEY;
                     db.addUser(user, ref, temRes, offer, API_KEY);
                 });
+
                 for (var i in temRes) {
                     db.getPublicPaths(temRes[i], function(paths) {
                         mainSrv.newUser(user, apiKey, paths);
                     });
-
                 }
             });
-            // console.log(JSON.stringify(offer, null, 2));
+
             res.send("API Server Woking!");
         });
-    }
-});
-
-app.post('/notifications/buyers',function(request, response, next) {
-    if (request.originalUrl !== '/notifications/buyers')
-        response.status(404).send('Cannot POST ' + request.originalUrl);
-    else {
-        var body = '';
-        if (request.get('Content-Type') === 'application/json') {
-            request.on('data', function(data) {
-                body += data;
-            });
-
-            request.on('end', function() {
-                // TODO: JSON.parse err
-                body = JSON.parse(body);
-                var r = body.resources;
-                // Checks proxy's resource in the request resources list
-                for (i in r) {
-                    if (r[i].name === resource.name
-                        && r[i].version === resource.version
-                        && r[i].url === resource.url) {
-                        console.log("[LOG] Resource OK!");
-                        mainSrv.newUser(body.customer, body.customer_name, body.reference, body.offering);
-                        return;
-                    }
-                }
-                console.log("[LOG] Resource FAIL!");
-            });
-        }
-        response.send('Server 2');
     }
 });
 

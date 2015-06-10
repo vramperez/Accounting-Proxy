d4d0b391f54a03a23b19b9c7dcd7a9b65288edfd
JesusMtnez <jmartinez@conwet.com>
2015-06-10 12:46:23 +0200

    Update module 'notifier.js'.
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- notifier.js
+++ notifier.js
@@ -1,86 +1,105 @@
-/**
- * Author: Jesús Martínez-Barquero Herrada
- * Last updated: 10 March 2015
- */
-
-/* Requires */
 var http = require('http');
-var sql = require('./sql.backup');
-var config = require('./config');
+var db = require('./db.js');
 var info = require('./HTTP_Client/info.json');
+var config = require('./config.json');
 
-/**
- * Send a request to WStore with accounting information of user.
- * @param {INTEGER}  i        [callback information]
- * @param {OBJECT}   user     [user object information]
- * @param {STRING}   userID   [user's id]
- * @param {FUNCTION} callback [callback to return request number]
- */
-exports.notify = function(i, user, userID, callback) {
-    if (user.requests !== 0) {
-        sql.getOfferInfo(user.reference, function(data) {
-            info.offering = data;
-            info.customer = userID;
-            info.time_stamp = (new Date()).toISOString();
-            info.correlation_number = user.num;
-            info.record_type = config.record_type;
-            info.unit = config.unit;
-            info.value = user.requests.toString();
-            info.component_label = config.component_label;
-            // Debug:
-            // console.log(info);
-            sendNotification(i, user, userID, callback)
-        });
-    }
-    else {
+exports.notify = function(user, API_KEY, num, callback) {
+    console.log(user, API_KEY, num);
+    if (num === 0){
         console.log('[LOG] NO request needed.');
-        callback(i, userID, 0, user.correlation_number);
-    }
-}
+        callback(user, API_KEY, 0);
+    } else {
+        console.log('[LOG] Request needed.');
 
-/**
- * Send notification to accounting server
- * @param {INTEGER} i        [callback information]
- * @param {OBJECT}  user     [user object]
- * @param {STRING}  userID   [user's id]
- * @param {OBJECT}  body     [JSON based on 'info.json']
- */
-var sendNotification = function(i, user, userID, callback) {
-    // Parse info to create bodo for the post
-    var body = JSON.stringify(info);
-    // Configure post options
-    var options = {
-        host: config.accounting_host,
-        port: config.accounting_port,
-        path: '/api/contracting/' + body.reference + '/accounting',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': body.length
-        }
+        var body = JSON.stringify(info);
+        //TODO: fill info
+        console.log(config.accounting_host, config.accounting_port);
+        var options = {
+            host: config.accounting_host,
+            port: config.accounting_port,
+            // TODO: get reference form DB
+            path: '/api/contracting/' + '1234' + '/accounting',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length
+            }
+        };
+
+        var request = http.request(options, function(res) {
+            if (200 <= res.statusCode && res.statusCode <= 299) {
+                console.log('[LOG] Resquest worked!');
+                db.resetCount(user, API_KEY);
+                callback(user, API_KEY, 0);
+            } else {
+                console.log('[LOG] Resquest failed!');
+                callback(user, API_KEY, num);
+            }
+        });
+
+        request.end();
     }
-    // Prepare request
-    var request = http.request(options, function(res) {
-        // Check response status code is valid
-        // Debug:
-        // if (true || ((200 <= res.statusCode) && (res.statusCode <= 299))) {
-        if ((200 <= res.statusCode) && (res.statusCode <= 299)) {
-            console.log('[LOG] Request WORK!');
-            sql.resetRequest(user.userID, user.correlation_number+1);
-            callback(i, userID, 0, user.correlation_number+1);
-        }
-        else  {
-            console.log('[LOG] Request FAIL!');
-            callback(i, userID, info.value, user.correlation_number);
-        }
-    });
-    // Request error handler
-    request.on('error', function(e) {
-        console.log('[LOG] Request FAIL!');
-        callback(i, userID, info.value, user.correlation_number);
-    });
-    // Send request
-    request.write(body);
-    // Finish request
-    request.end();
-}
+};
+
+// DEPRECATED
+// var sendNotification = function(i, user, userID, callback) {
+//     // Parse info to create bodo for the post
+//     var body = JSON.stringify(info);
+//     // Configure post options
+//     var options = {
+//         host: config.accounting_host,
+//         port: config.accounting_port,
+//         path: '/api/contracting/' + body.reference + '/accounting',
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Content-Length': body.length
+//         }
+//     };
+//     // Prepare request
+//     var request = http.request(options, function(res) {
+//         // Check response status code is valid
+//         // Debug:
+//         // if (true || ((200 <= res.statusCode) && (res.statusCode <= 299))) {
+//         if ((200 <= res.statusCode) && (res.statusCode <= 299)) {
+//             console.log('[LOG] Request WORK!');
+//             sql.resetRequest(user.userID, user.correlation_number+1);
+//             callback(i, userID, 0, user.correlation_number+1);
+//         }
+//         else  {
+//             console.log('[LOG] Request FAIL!');
+//             callback(i, userID, info.value, user.correlation_number);
+//         }
+//     });
+//     // Request error handler
+//     request.on('error', function(e) {
+//         console.log('[LOG] Request FAIL!');
+//         callback(i, userID, info.value, user.correlation_number);
+//     });
+//     // Send request
+//     request.write(body);
+//     // Finish request
+//     request.end();
+// };
+// DEPRECATED
+// exports.notify = function(i, user, userID, callback) {
+//     if (user.requests !== 0) {
+//         sql.getOfferInfo(user.reference, function(data) {
+//             info.offering = data;
+//             info.customer = userID;
+//             info.time_stamp = (new Date()).toISOString();
+//             info.correlation_number = user.num;
+//             info.record_type = config.record_type;
+//             info.unit = config.unit;
+//             info.value = user.requests.toString();
+//             info.component_label = config.component_label;
+//             // Debug:
+//             // console.log(info);
+//             sendNotification(i, user, userID, callback)
+//         });
+//     }
+//     else {
+//         callback(i, userID, 0, user.correlation_number);
+//     }
+// };
+

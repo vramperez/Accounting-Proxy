dcbeef1e33dd9d221d5692e0bac8661902818036
Victor Ramperez <vramperez@conwet.com>
2015-09-24 13:46:40 +0200

    resources methods implemented
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- db_Redis.js
+++ db_Redis.js
@@ -9,6 +9,10 @@ db.on('error', function(err){
 	console.log("Error" + err);
 });
 
+exports.loadFromDB = function(setData) { 
+
+};
+
 
 exports.newService = function(path, port, callback) {
 
@@ -50,20 +54,99 @@ exports.mapService = function(publicPath, privatePath, port, callback){
 };
 
 
-/*exports.addResource = function(data, callback) {
+exports.addResource = function(data, callback) {
 
-	db.sadd('resources', data.publicPath, function(err, reply){
-		console.log(reply);
+	db.sadd(['resources', data.publicPath + data.offering.organization + data.offering.name + data.offering.version], function(err) {
+		if(err)
+			callback(err);
+		else{
+			db.hmset(data.publicPath + data.offering.organization + data.offering.name + data.offering.version, {
+					'publicPath': data.publicPath,
+					'org': data.offering.organization,
+					'name': data.offering.name,
+					'version': data.offering.version,
+					'record_type': data.record_type,
+					'unit': data.unit,
+					'component_label': data.component_label
+				}, function(err){
+					if(err)
+						callback(err);
+				});
+		}
+	});
+};
+
+exports.loadUnits = function(callback) {
+
+	db.smembers('resources', function(err, resources) {
+		if(err)
+			callback(err);
+		else {
+			var toReturn = {};
+			resources.forEach(function(entry) {
+				db.hgetall(entry, function(err, obj) {
+					if(err)
+						callback(err);
+					else {
+						toReturn[obj['publicPath']] = {
+							publicPath: obj['publicPath'],
+							organization: obj['org'],
+							name: obj['name'],
+							version: obj['version'],
+							unit: obj['unit']
+						}
+						callback(toReturn);
+					}
+				});
+			});
+		}
+	})
+};
+
+exports.loadResources = function(callback) {
+
+	db.smembers('resources', function(err, resources) {
+
+		var toReturn = {};
+		var fields = ['privatePath', 'port'];
+		resources.forEach(function(entry) {
+			db.hget(entry, 'publicPath', function(err, obj) {
+				if(err)
+					callback(err)
+				else{
+					db.hgetall(obj, function(err, obj) {
+						if(err)
+							callback(err)
+						else {
+							toReturn = {
+								privatePath: obj['privatePath'],
+								port: obj['port']
+							};
+							callback(toReturn);
+						}
+					});
+				}
+			});
+		});
+	});
+};
+
+
+exports.addInfo = function(API_KEY, data, callback) {
+
+	db.hmset(API_KEY, {
+		'organization': data.organization,
+		'name': name: data.name,
+		'version': version: data.version,
+		'actorID': actorID: data.actorID,
+		'API_KEY': API_KEY,
+		'ref': ref: data.reference
+	}, function(err) {
+		if(err)
+			callback(err);
+		else{
+			//Añadir a accoun ting
+		}
 	});
 
-	db.hmset(data.publicPath, { //Similar to offerResource
-		'organization': data.offering.organization,
-		'name': data.offering.name,
-		'version': data.offering.version,
-		'record_type': data.record_type,
-		'unit': data.unit,
-		'component_label': data.component_label
-	}, function(err){
-		console.log(err);
-	});
-};*/
+};

83a72509ce65c5be2198437e9f0153aa6f6ba7fb
Victor Ramperez <vramperez@conwet.com>
2015-09-28 13:33:18 +0200

    loadFromDB implemented and addInfo modified
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- db_Redis.js
+++ db_Redis.js
@@ -9,8 +9,54 @@ db.on('error', function(err){
 	console.log("Error" + err);
 });
 
-exports.loadFromDB = function(setData) { 
+exports.init = function() {
+	//Not necessary
+};
 
+exports.loadFromDB = function(setData) { 
+	var data = {};
+
+	db.smembers('resources', function(err, resources) {
+		if(err)
+			setData(err, null);
+		else
+			db.smembers('API_KEYS', function(err, api_keys) {
+				api_keys.forEach(function(api_key) {
+					db.hgetall(api_key, function(err, acc){
+						resources.forEach(function(resource) {
+							db.hgetall(resource, function(err, res) {
+								if(acc.organization === res.organization &&
+									acc.name === res.name &&
+									acc.version === res.version){
+										db.hgetall(acc.actorID + acc.API_KEY + res.publicPath, function(err, account) {
+											db.hgetall(res.publicPath, function(err, serv) {
+												data[acc.API_KEY] = {
+													actorID: acc.actorID,
+													organization: acc.organization,
+													name: acc.name,
+													version: acc.version,
+													accounting: {},
+													reference: acc.reference
+												}
+											data[acc.API_KEY].accounting[res.publicPath] = {
+												privatePath: serv.privatePath,
+												port: serv.port,
+												num: account.num,
+												correlation_number: account.correlation_number,
+												unit: res.unit
+											}
+											setData(null, data);
+										});
+									});
+					
+								}
+							});
+						});
+					});
+				});
+	
+			});
+	});
 };
 
 
@@ -26,7 +72,37 @@ exports.newService = function(path, port, callback) {
 
 // CLI: deleteService [path] [port]
 exports.deleteService = function(path, port, callback) {
-
+	
+	db.srem(path, port, function(err) {
+		if(err)
+			callback("[ERROR] Deleting service failed.");
+		else {
+			db.smembers(path, function(err, obj) {
+				if(obj.length === 0)
+					db.del(path, function(err) {
+						if(err)
+							callback("[ERROR] Deleting service failed.");
+						else
+							db.del(path);
+					});
+				db.smembers('public', function(err, publics) {
+					publics.forEach(function(entry) {
+						db.hgetall(entry, function(err, service) {
+							if(err)
+								callback(err)
+							else{
+								if(service.privatePath === path &&
+									service.port === port){
+										db.del(entry);
+										db.srem('public', entry);
+								}
+							}
+						});
+					});
+				});
+			});
+		}
+	});
 };
 
 // CLI: getService [publicPath]
@@ -37,29 +113,33 @@ exports.getService = function(publicPath, callback) {
 			callback(err);
 		else
 			callback(obj);
-	})
+	});
 };
 
 // CLI: mapService [publicPath] [privatePath] [port]
 exports.mapService = function(publicPath, privatePath, port, callback){
 
-	db.smembers(privatePath, function(err, reply) {
+	db.sadd('public', publicPath, function(err) {
 		if(err)
-			callback(err);
-		else {
-			if (reply[reply.indexOf(port)] === undefined)
-				callback(undefined)
-			else
-				db.hmset(publicPath, {
-				'privatePath': privatePath,
-				'port': port,
-			}, function(err) {
+			callback(err)
+		else
+			db.smembers(privatePath, function(err, reply) {
 				if(err)
 					callback(err);
+				else {
+					if (reply[reply.indexOf(port)] === undefined)
+						callback(undefined)
+					else
+						db.hmset(publicPath, {
+						'privatePath': privatePath,
+						'port': port,
+					}, function(err) {
+						if(err)
+							callback(err);
+					});
+				}
 			});
-		}
-	});
-
+		});
 };
 
 
@@ -101,7 +181,7 @@ exports.loadUnits = function(callback) {
 					else {
 						toReturn[obj['publicPath']] = {
 							publicPath: obj['publicPath'],
-							organization: obj['org'],
+							organization: obj['organization'],
 							name: obj['name'],
 							version: obj['version'],
 							unit: obj['unit']
@@ -145,40 +225,45 @@ exports.loadResources = function(callback) {
 
 exports.addInfo = function(API_KEY, data, callback) {
 
-	db.hmset(API_KEY, {
-		'organization': data.organization,
-		'name': data.name,
-		'version': data.version,
-		'actorID': data.actorID,
-		'API_KEY': API_KEY,
-		'ref': data.reference
-	}, function(err) {
-		if(err) {
+	db.sadd(['API_KEYS', API_KEY], function(err) {
+		if(err)
 			callback(err);
-		} else {
-			db.sadd([data.actorID, API_KEY], function(err){
+		else
+			db.hmset(API_KEY, {
+				'organization': data.organization,
+				'name': data.name,
+				'version': data.version,
+				'actorID': data.actorID,
+				'API_KEY': API_KEY,
+				'reference': data.reference
+			}, function(err) {
 				if(err) {
 					callback(err);
 				} else {
-					var acc;
-					for (var p in data.accounting) {
-						acc = data.accounting[p];
-						db.hmset(data.actorID + API_KEY + p, {
-							'actorID': data.actorID,
-							'API_KEY': API_KEY,
-							'num': acc.num,
-							'publicPath': p,
-							'correlation_number': acc.correlation_number
-						}, function(err){
-							if(err)
-								callback(err);
-							else
-								callback();
-						});
-					};
+					db.sadd([data.actorID, API_KEY], function(err){
+						if(err) {
+							callback(err);
+						} else {
+							var acc;
+							for (var p in data.accounting) {
+								acc = data.accounting[p];
+								db.hmset(data.actorID + API_KEY + p, {
+									'actorID': data.actorID,
+									'API_KEY': API_KEY,
+									'num': acc.num,
+									'publicPath': p,
+									'correlation_number': acc.correlation_number
+								}, function(err){
+									if(err)
+										callback(err);
+									else
+										callback();
+								});
+							};
+						}
+					});
 				}
 			});
-		}
 	});
 };
 
@@ -231,4 +316,31 @@ exports.getInfo = function(user, callback) {
 			});
 		}
 	});
+};
+
+exports.count = function(actorID, API_KEY, publicPath, amount) {
+    db.hget(actorID + API_KEY + publicPath, 'num', function(err, num) {
+    	if(err)
+    		console.log(err)
+    	else{
+    		num = parseFloat(num) + amount;
+    		db.hmset(actorID + API_KEY + publicPath, {
+    			'num' : num
+    		});
+    	}
+    });
+};
+
+exports.resetCount = function(actorID, API_KEY, publicPath) {
+	db.hget(actorID + API_KEY + publicPath, 'correlation_number', function(err, correlation_number) {
+    	if(err)
+    		console.log(err)
+    	else{
+    		correlation_number = parseInt(correlation_number) + 1;
+    		db.hmset(actorID + API_KEY + publicPath, {
+    			'correlation_number' : correlation_number,
+    			'num' : 0
+    		});
+    	}
+    });
 };
